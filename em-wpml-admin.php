785053c03ac533d4b8805254ad38164a7bbedae5
netweblogic <netweblogic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-10 16:43:41 +0000

    updating to v1.0
    git-svn-id: https://plugins.svn.wordpress.org/events-manager-wpml/trunk@1217437 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- em-wpml-admin.php
+++ em-wpml-admin.php
@@ -1,71 +1,59 @@
 <?php
-class EM_WPML_Admin{
+class EM_WPML_Admin {
+    
 	public static function init(){
-	    if( !class_exists('SitePress') || !defined('EM_VERSION') ) return; //only continue of both EM and WPML are activated
 		global $pagenow;
-		add_filter('em_event_save_meta_pre','EM_WPML_Admin::event_save_meta_pre',10,2);
-		add_filter('em_location_save_pre','EM_WPML_Admin::location_save_pre',10,2);
-		add_action('add_meta_boxes', 'EM_WPML_Admin::meta_boxes',100);
-	}
-	
-	public static function meta_boxes(){
-	    global $EM_Event, $sitepress;
-	    //decide if it's a master event, if not then hide the meta boxes
-	    if( !EM_WPML::is_master_event($EM_Event) ){
-		    remove_meta_box('em-event-when', EM_POST_TYPE_EVENT, 'side');
-	    	remove_meta_box('em-event-where', EM_POST_TYPE_EVENT, 'normal');
-		    remove_meta_box('em-event-bookings', EM_POST_TYPE_EVENT, 'normal');
-		    remove_meta_box('em-event-bookings-stats', EM_POST_TYPE_EVENT, 'side');
-		    add_meta_box('em-event-wpml', __('Translated Event Information','dbem'), array('EM_WPML_Admin','event_meta_box'),EM_POST_TYPE_EVENT, 'side','high');
-	    }
-	}
-	
-	public static function event_meta_box(){
-	    global $EM_Event;
-	    ?>
-	    <input type="hidden" name="_emnonce" value="<?php echo wp_create_nonce('edit_event'); ?>" />
-	    <p>
-	    	<?php
-	    	if( !empty($EM_Event->event_id) ){
-		    	$master_event_link = em_get_event(EM_WPML::get_master_event_id($EM_Event->event_id))->get_edit_url();
-		    }elseif( !empty($_REQUEST['trid']) ){
-				$post_id = EM_WPML::get_trid_original_post_id($_REQUEST['trid']);
-				$master_event_link = em_get_event($post_id,'post_id')->get_edit_url();
-			}
-			echo sprintf(__('This is a translated event, therefore your time, location and booking information is handled by your <a href="%s">originally created event</a>.', 'em-wpml'), $master_event_link);
-	    	?>
-	    </p>
-	    <?php
-	}
-
-	/**
-	 * Saves a new location index record if this is a new languge addition
-	 * @param boolean $result
-	 * @param EM_Location $EM_Location
-	 */
-	public static function location_save_pre($EM_Location){
-		global $wpdb;
-	}
-
-	/**
-	 * Saves a new location index record if this is a new languge addition
-	 * @param boolean $result
-	 * @param EM_Event $EM_Event
-	 */
-	public static function event_save_meta_pre($EM_Event){
-		global $wpdb, $post;
-		if( $EM_Event->post_id != $post->ID ){
-			//different language, make sure we don't have the same event_id as the original language
-			$event = em_get_event($EM_Event->event_id); //gets the true event
-			if( $EM_Event->post_id == $event->post_id ){
-				//we have a dupe, so we need to reset the event id and the post id here
-				$EM_Event->post_id = $post->ID;
-				$EM_Event->event_id = null;
-				update_post_meta($post->ID, '_post_id', $post->ID);
-				update_post_meta($post->ID, '_event_id', '');
-				$EM_Event->load_postdata($post,'post_id');
-			}
+		add_filter('em_ml_admin_original_event_link','EM_WPML_Admin::em_ml_admin_original_event_link',10,1);
+		//recurring events notice
+		if( !get_option('em_wpml_disable_recurrence_notice') && is_admin() && current_user_can('activate_plugins') ){
+		    global $pagenow;
+		    if( !empty($_REQUEST['em_wpml_disable_recurrence_notice']) ){
+		        update_option('em_wpml_disable_recurrence_notice',true);
+		    }else{
+			    add_action('admin_notices','EM_WPML_Admin::disable_recurrence_notice');
+		    }
+		}
+		if( $pagenow == 'edit.php' && !empty($_REQUEST['page']) && $_REQUEST['page'] == 'events-manager-options' ){
+		    add_filter('admin_init', 'EM_WPML_Admin::options_redirect');
 		}
 	}
+	
+	public static function options_redirect(){
+	    global $sitepress;
+	    //check if we've redirected already
+	    if( !empty($_REQUEST['wpmlredirect']) ){
+	        global $EM_Notices; /* @var $EM_Notices EM_Notices */
+	        $EM_Notices->add_info(__('You have been redirected to the main language of your site for this settings page. All translatable settings can be translated here.', 'events-manager-wpml'));
+	    }
+	    //redirect users if this isn't the main language of the blog
+	    if( EM_ML::$current_language == EM_ML::$wplang ) return;
+	    $sitepress_langs = $sitepress->get_active_languages();
+	    foreach( $sitepress_langs as $lang => $lang_info ){
+	        if( $lang_info['default_locale'] == EM_ML::$wplang ){
+                wp_redirect(admin_url('edit.php?post_type=event&page=events-manager-options&wpmlredirect=1&lang='.$lang));
+                exit();
+	        }
+	    }
+	}
+    
+    /**
+     * Notifies user of the fact that recurrences are disabled by default with this plugin activated 
+     */
+    public static function disable_recurrence_notice(){
+		?>
+		<div id="message" class="updated">
+			<p><?php echo sprintf(__('Since you are using WPML, we have automatically disabled recurring events, your recurrences already created will act as normal single events. This is because recurrences are not compatible with WPML at the moment. If you really still want recurrences enabled, then you should add %s to your wp-config.php file. <a href="%s">Dismiss</a>','events-manager-wpml'),'<code>define(\'EM_WMPL_FORCE_RECURRENCES\',true);</code>', add_query_arg(array('em_wpml_disable_recurrence_notice'=>1))); ?></p>
+		</div>
+		<?php
+    }
+	
+	public static function em_ml_admin_original_event_link( $link ){
+	    global $EM_Event, $sitepress;
+    	if( empty($EM_Event->event_id) && !empty($_REQUEST['trid']) ){
+			$post_id = $sitepress::get_original_element_id_by_trid($_REQUEST['trid']);
+			$original_event_link = em_get_event($post_id,'post_id')->get_edit_url();
+		}
+		return $link;
+	}
 }
-add_action('plugins_loaded', 'EM_WPML_Admin::init');
+EM_WPML_Admin::init();
